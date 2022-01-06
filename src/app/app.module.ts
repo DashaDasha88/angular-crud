6d689b0001e9934e9fc7730ed06de01daabb21b9
Dasha <dasha.kotova.mail@gmail.com>
2022-01-05 21:12:49 -0500

    add login and signup components and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeedashboardComponent } from './employeedashboard/employeedashboard.component';
 import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeedashboardComponent
+    EmployeedashboardComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
